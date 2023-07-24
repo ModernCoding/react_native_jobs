ecd65a7d1cf00d4d8e7406ebe9e6f63dbe13d27f
Michel Boretti <contact@coding-crafting-consulting.com>
2023-07-24 21:40:39 +0700

    nearbyjobs

--- components/common/cards/nearby/NearbyJobCard.jsx
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
 
 import styles from './nearbyjobcard.style'
+import { checkImageURL } from '../../../../utils'
 
-const NearbyJobCard = () => {
+const NearbyJobCard = ({ job, handleNavigate }) => {
   return (
-    <View>
-      <Text>NearbyJobCard</Text>
-    </View>
+    <TouchableOpacity
+      style={ styles.container }
+      onPress={handleNavigate}
+    >
+      <TouchableOpacity
+        style={ styles.logoContainer }
+      >
+        <Image
+          source={{ uri: checkImageURL (job.employer_logo) }}
+          resizeMode='contain'
+          style={ styles.logoImage }
+        />
+      </TouchableOpacity>
+
+      <View style={ styles.infoContainer }>
+        <Text
+          style={ styles.jobName }
+          numberOfLines={ 1 }
+        >
+          { job.job_title }
+        </Text>
+
+        <Text style={ styles.location }>
+          { job.job_country }
+        </Text>
+
+      </View>
+
+    </TouchableOpacity>
   )
 }
 
