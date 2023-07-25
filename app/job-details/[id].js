4ea14a9b1ce7c905b97cdfe7fcc7b80ccd8e523b
Michel Boretti <contact@coding-crafting-consulting.com>
2023-07-25 16:13:29 +0700

    job details

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -31,6 +31,11 @@ const JobDetails = () => {
     job_id: params.id
   })
 
+  const [ refreshing, setRefreshing ] = useState (false)
+
+  const onRefresh = () => {}
+
+
   return (
     <SafeAreaView
       style={{ flex: 1, backgroundColor: COLORS.lightWhite }}
@@ -55,9 +60,59 @@ const JobDetails = () => {
           ),
           headerTitle: ""
         }}
-      >
+      />
 
-      </Stack.Screen>
+      <>
+        <ScrollView
+          showsVerticalScrollIndicator={ false }
+          refreshControl={
+            <RefreshControl
+              refreshing={ refreshing }
+              onRefresh={ onRefresh }
+            />
+          }
+        >
+          { 
+            isLoading
+            
+              ? (
+                  <ActivityIndicator
+                    size="large"
+                    color={ COLORS.primary }
+                  />
+                )
+
+              : error
+                
+                ? (<Text>Something went wrong</Text>)
+                
+                : data.length === 0
+                
+                  ? (<Text>No data</Text>)
+                  
+                  : (
+                      <View
+                        style={{
+                          padding: SIZES.medium,
+                          paddingBottom: 100
+                        }}
+                      >
+                        <Company
+                          companyLogo={ data [ 0 ].employer_logo }
+                          jobTitle={ data [ 0 ].job_title }
+                          companyName={ data [ 0 ].employer_name }
+                          location={ data [ 0 ].job_country }
+                        />
+
+                        <JobTabs
+                        />
+                      </View>  
+                    )
+
+          }
+        </ScrollView>
+      </>
+
     </SafeAreaView>
   )
 }
